6500076445b7fe57f363ede106950fabe28d3144
GhifarDEvSec <algi6unawan@gmail.com>
2026-07-18 08:03:52 +0700

    FIXXX

--- Dent-In-main/src/main/java/com/dentist/config/WebConfig.java
+++ Dent-In-main/src/main/java/com/dentist/config/WebConfig.java
@@ -12,11 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
     private String allowedOrigins;
 
     @Override
-public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
-        .allowedOriginPatterns(allowedOrigins.split(","))
-        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-        .allowedHeaders("*")
-        .allowCredentials(true);
- }
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOriginPatterns(allowedOrigins.split(","))
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
 }
